43a8215e2e9a82191f7d019b6abafe9ba269c785
sheehan <sheehansuneel5@gmail.com>
2022-06-24 13:31:37 +0100

    first commit

---
+++ models/connection.js
@@ -0,0 +1,10 @@
+var pg = require('pg');
+
+const connectionString = process.env.DATABASE_URL
+const Pool = pg.Pool
+const pool = new Pool({
+    connectionString,
+    max: 10      
+})
+
+module.exports = pool;
